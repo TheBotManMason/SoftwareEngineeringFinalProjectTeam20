10c2e4efdf281b78640b9a9217007ffaecdffb5b
cpeter66 <cpeter66@charlotte.edu>
2025-11-06 16:23:04 -0500

    Update and rename sandwiches.py to customers.py
    Example for the changes that need to be made to the rest of the models and the model loaders

---
+++ ITSC3155-Assignments-main/ITSC3155-Assignments-main/FinalProject/api/models/customers.py
@@ -0,0 +1,16 @@
+from sqlalchemy import Column, ForeignKey, Integer, String, DECIMAL, DATETIME
+from sqlalchemy.orm import relationship
+from datetime import datetime
+from ..dependencies.database import Base
+
+
+class Customer(Base):
+    __tablename__ = "customers"
+
+    id = Column(Integer, primary_key=True, index=True, autoincrement=True)
+    name = Column(String(100), nullable=False)
+    email = Column(String(100), unique=True, nullable=True)
+    phone_number = Column(String(20), nullable=True)
+    orders = relationship("Order", back_populates="customer")
+    reviews = relationship("Review", back_populates="customer)
+    
