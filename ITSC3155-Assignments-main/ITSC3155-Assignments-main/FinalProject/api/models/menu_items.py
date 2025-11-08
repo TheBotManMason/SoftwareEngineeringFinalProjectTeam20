18d9d359dd03dd09c29a81450d070ca4a6af078d
EvanReichstetter <ereichst@charlotte.edu>
2025-11-07 21:01:47 -0500

    Renamed items to menu_items

--- ITSC3155-Assignments-main/ITSC3155-Assignments-main/FinalProject/api/models/menu_items.py
+++ ITSC3155-Assignments-main/ITSC3155-Assignments-main/FinalProject/api/models/menu_items.py
@@ -3,13 +3,13 @@ from sqlalchemy.orm import relationship
 from datetime import datetime
 from ..dependencies.database import Base
 
-class Item(Base):
-    __tablename__ =  "items"
+class MenuItem(Base):
+    __tablename__ =  "menu_items"
 
     id = Column(Integer, primary_key=True, index=True, autoincrement=True)
     name = Column(String(100), nullable=False)
     description = Column(String(500), nullable=True)
     price = Column(DECIMAL(10,2), nullable=False)
     available = Column(Boolean, nullable=False, default=True)
-    order_items = relationship("OrderItem", back_populates="item")
-    reviews = relationship("Review", back_populates="item")
+    order_items = relationship("OrderItem", back_populates="menuitem")
+    reviews = relationship("Review", back_populates="menuitem")
