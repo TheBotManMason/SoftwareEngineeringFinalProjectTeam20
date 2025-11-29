9bf21ee77f8810074077e7a0535c62ff4b632c78
cpeter66 <cpeter66@charlotte.edu>
2025-11-29 17:30:50 -0500

    Christian finishing working on routers, done customers ingredients and orders. What routers remain are menu_items router, order_items router, promotions router, reviews router, updating index.py in routers

--- ITSC3155-Assignments-main/ITSC3155-Assignments-main/FinalProject/api/routers/index.py
+++ ITSC3155-Assignments-main/ITSC3155-Assignments-main/FinalProject/api/routers/index.py
@@ -1,12 +1,4 @@
-from . import orders, order_details
+from . import orders, ingredients, customers
 
 
 def load_routes(app):
-    app.include_router(customers.router)
-    app.include_router(ingredients.router)
-    app.include_router(menu_items.router)
-    app.include_router(orders.router)
-    app.include_router(order_items.router)
-    app.include_router(promotions.router)
-    app.include_router(reviews.router)
-    app.include_router(analytics.router)
--- ITSC3155-Assignments-main/ITSC3155-Assignments-main/FinalProject/api/routers/orders.py
+++ ITSC3155-Assignments-main/ITSC3155-Assignments-main/FinalProject/api/routers/orders.py
@@ -1,34 +1,34 @@
-from fastapi import APIRouter, Depends, FastAPI, status, Response
+from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
 from ..controllers import orders as controller
 from ..schemas import order as schema
-from ..dependencies.database import engine, get_db
+from ..dependencies.database import get_db
 
-router = APIRouter(
-    tags=['Orders'],
-    prefix="/orders"
-)
-
+router = APIRouter(tags=['Orders'], prefix="/orders")
 
 @router.post("/", response_model=schema.Order)
 def create(request: schema.OrderCreate, db: Session = Depends(get_db)):
     return controller.create(db=db, request=request)
 
-
 @router.get("/", response_model=list[schema.Order])
 def read_all(db: Session = Depends(get_db)):
     return controller.read_all(db)
-
 
 @router.get("/{item_id}", response_model=schema.Order)
 def read_one(item_id: int, db: Session = Depends(get_db)):
     return controller.read_one(db, item_id=item_id)
 
+@router.get("/track/{tracking_number}", response_model=schema.Order)
+def read_one(tracking_number: str, db: Session = Depends(get_db)):
+    return controller.get_by_tracking(db, tracking_number=tracking_number)
 
 @router.put("/{item_id}", response_model=schema.Order)
 def update(item_id: int, request: schema.OrderUpdate, db: Session = Depends(get_db)):
-    return controller.update(db=db, request=request, item_id=item_id)
+    return controller.update(db=db, item_id=item_id, request=request)
 
+@router.put("/{item_id}/status", response_model=schema.Order)
+def update_status(item_id: int, status: str, db: Session = Depends(get_db)):
+    return controller.update_status(db, item_id=item_id, status=status)
 
 @router.delete("/{item_id}")
 def delete(item_id: int, db: Session = Depends(get_db)):
